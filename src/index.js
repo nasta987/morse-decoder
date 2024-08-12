9bec7a4eb1f4b37a50e238ac35ce7bedbef376ec
nasta987 <99967637+nasta987@users.noreply.github.com>
2024-08-12 16:32:08 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let arraySt= [];
+    for (let i=0; i < expr.length; i+=10) {
+      arraySt.push(expr.slice(i, i+10))
+    }
+    console.log(arraySt);
+
+let arrayMorse = arraySt.map (elementSt => {
+  let decoderElement ="";
+  
+  if (elementSt === "**********") {
+return " "
+  } 
+  for (let j=0; j< elementSt.length; j+=2) {
+    let decoderCouple = elementSt.slice(j, j + 2);
+            if (decoderCouple === '10') {
+                decoderElement += '.';
+            } else if (decoderCouple === '11') {
+                decoderElement += '-';
+            } else {
+                decoderElement += '';
+            }
+        }
+        return decoderElement;
+    });
+
+    console.log(arrayMorse);
+    let morseString = "";
+    
+    for (let z =0; z < arrayMorse.length; z++) {
+
+ if (arrayMorse[z] === " ") {
+            morseString += " ";
+        } else {
+            morseString += MORSE_TABLE[arrayMorse[z]] || '';
+        }
+ }
+
+    console.log(morseString);
+    return morseString;
+
 }
 
 module.exports = {
